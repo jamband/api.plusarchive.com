86aecfba2eada1a2dafe7574d4e22c59397a5130
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-21 15:33:25 +0900

    Improve constants for test environment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,8 +11,8 @@
 
 declare(strict_types=1);
 
-define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'test');
 
 require __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
