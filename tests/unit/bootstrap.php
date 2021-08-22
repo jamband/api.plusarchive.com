1dd222aa23cf6ef1802a6908d13410f7a749e6ab
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-22 15:45:25 +0900

    Use require instead of require_once

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -15,7 +15,7 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 
 require __DIR__.'/../../vendor/autoload.php';
-require_once __DIR__.'/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__.'/../../vendor/yiisoft/yii2/Yii.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
 $dotenv->load();
