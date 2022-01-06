49affe3a9f0eef74cc6db5051b608f83b4a49fb3
Tomoki Morita <tmsongbooks215@gmail.com>
2022-01-07 01:35:29 +0900

    Improve the application configuration on unit testing

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -2,17 +2,24 @@
 
 declare(strict_types=1);
 
-use yii\helpers\ArrayHelper;
+use app\components\Hashids;
 use yii\web\Application;
 
 require __DIR__.'/../bootstrap.php';
 
-new Application(ArrayHelper::merge(require __DIR__.'/../../config/base.php', [
+new Application([
     'id' => 'test',
+    'basePath' => dirname(__DIR__, 2),
     'components' => [
-        'db' => null,
+        'formatter' => [
+            'dateFormat' => 'yyyy.MM.dd',
+            'datetimeFormat' => 'yyyy.MM.dd HH:mm',
+        ],
         'hashids' => [
+            'class' => Hashids::class,
             'salt' => 'test',
+            'minHashLength' => 11,
+            'alphabet' => 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-',
         ],
     ],
-]));
+]);
