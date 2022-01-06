8ec0b7c3649185368761ce0da4acdf382cefa562
Tomoki Morita <tmsongbooks215@gmail.com>
2022-01-07 00:29:01 +0900

    Improve bootstrap file for testing

--- tests/console/bootstrap.php
+++ tests/console/bootstrap.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 use yii\console\Application;
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
-
-require __DIR__.'/../../vendor/autoload.php';
-require __DIR__.'/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__.'/../bootstrap.php';
 
 new Application([
-    'id' => 'test-console',
+    'id' => 'test',
     'basePath' => dirname(__DIR__, 2),
     'controllerNamespace' => 'app\commands',
     'enableCoreCommands' => false,
--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -2,20 +2,13 @@
 
 declare(strict_types=1);
 
-use Dotenv\Dotenv;
 use yii\helpers\ArrayHelper;
 use yii\web\Application;
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
-
-require __DIR__.'/../../vendor/autoload.php';
-require __DIR__.'/../../vendor/yiisoft/yii2/Yii.php';
-
-Dotenv::createImmutable(dirname(__DIR__, 2))->load();
+require __DIR__.'/../bootstrap.php';
 
 new Application(ArrayHelper::merge(require __DIR__.'/../../config/base.php', [
-    'id' => 'test-unit',
+    'id' => 'test',
     'components' => [
         'db' => null,
         'hashids' => [
